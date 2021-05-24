ac6e954502b2c8ea927482cf17a2cd6b07455bea
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-05-24 20:21:21 -0300

    observables

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
-  styleUrls: ['./basic.component.scss']
+  styleUrls: ['./basic.component.scss'],
 })
 export class BasicComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    const myFirstObservable = new Observable(
+      (observer: Observer<number>) => {
+        observer.next(1);
+        observer.next(2);
+        observer.next(3);
+        observer.next(4);
+        observer.next(5);
+        observer.error('error');
+        observer.complete();
+      }
+    );
+    myFirstObservable.subscribe(
+      (n: number) => {
+        console.log(n)
+      },
+      (error) => console.log(error),
+      () => console.log('completed')
+    )
   }
-
 }
